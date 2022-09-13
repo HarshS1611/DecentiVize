3f5ffaab4ce83a422c5431c147cbc05cd878f645
Harsh Singh <harshsingh16d@gmail.com>
2022-09-13 13:28:34 +0530

    contract created and successfully compiled and frontend template created

---
+++ frontend/src/blockchain/scripts/deploy.js
@@ -0,0 +1,25 @@
+const hre = require("hardhat");
+const fs = require("fs");
+
+async function main() {
+  const NFTContract = await hre.ethers.getContractFactory("NFTContract");
+  const nftContract = await NFTContract.deploy();
+  await nftContract.deployed();
+
+  console.log("NFTContract deployed to:", nftContract.address);
+  console.log(
+    `If it is in mumbai: https://mumbai.polygonscan.com/address/${nftContract.address}`
+  );
+
+  fs.writeFileSync(
+    "./config.js",
+    `export const nftAddress = "${nftContract.address}";`
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
